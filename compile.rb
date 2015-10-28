2672701b4378ae1e4a979021b99319183d722767
Jackson Oates <jackson.oates@gmail.com>
2015-10-27 22:41:49 -0700

    refactored, slect color code and call colorize method, no need to have methods for each color

--- compile.rb
+++ compile.rb
@@ -12,39 +12,15 @@ items = [
   "==> default: After this operation, 264 kB of additional disk space will be used.",
   "==> default: Get:1 http://ppa.launchpad.net/ondrej/php5/ubuntu/ precise/main php5-mysql i386 5.5.30+dfsg-1+deb.sury.org~precise+1 [80.8 kB]"
 ]
-color = [ :red, :green, :yellow]
+colors = [ 31, 32, 33, 34, 35, 36 ]
 class String
   def colorize(color_code)
     "\e[#{color_code}m#{self}\e[0m"
   end
-
-  def red
-    colorize(31)
-  end
-
-  def green
-    colorize(32)
-  end
-
-  def yellow
-    colorize(33)
-  end
-
-  def blue
-    colorize(34)
-  end
-
-  def pink
-    colorize(35)
-  end
-
-  def light_blue
-    colorize(36)
-  end
 end
 
 (0..10000).each do |i|
-  puts "#{items.sample}".send color.sample
+  puts "#{items.sample}".send(:colorize, colors.sample)
   sleep( rand(0.3..0.8) )
 end
 
